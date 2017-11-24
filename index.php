70eb83e07e826c05ee01f7cea958effffc53b08f
insomnux <insomnux@insomnus.info>
2017-11-24 10:40:40 +0200

    added CSS normalize - excerpt vs full post in index.php

--- index.php
+++ index.php
@@ -12,18 +12,24 @@ get_header(); ?>
 <?php
 if ( have_posts() ) {
   while ( have_posts() ) {
-    the_title( '<h1 class="post_title"><a href="' . esc_url( get_permalink() ) . '">', '</a></h1>' );
     the_post();
-    the_content();
-    wp_link_pages( array(
-      "before" => "<div>" . __( "Page: ", "inxboilerplate" ),
-      "after"  => "</div>",
-    ) );
-  }
-  the_posts_navigation();
+    if  ( is_singular() ) { /* if single post view */
+      the_title();
+      the_content();
+      wp_link_pages( array(
+        "before" => "<div>" . __( "Page: ", "inxboilerplate" ),
+        "after"  => "</div>",
+      ) );
+      the_posts_navigation();
+    } else { /* if index or archive page listing */
+      the_title( '<h1 class="post_title"><a href="' . esc_url( get_permalink() ) . '">', '</a></h1>' );
+      the_excerpt();
+    } /* end if singular */
+  }  /* end while */
+  
 } else {
   _e( "No posts found.", "inxboilerplate" );
-} // have_posts
+} /* have_posts */
 ?>
 
 
